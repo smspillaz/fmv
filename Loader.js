a6de6a0e230bf22b07bac0a1d2a72fe869124860
Sam Spilsbury <smspillaz@gmail.com>
2016-02-06 18:12:24 +0800

    Add viewer for emscripten mode

---
+++ Loader.js
@@ -0,0 +1,46 @@
+var Module = {
+    preRun: [],
+    postRun: [],
+    onRuntimeInitialized: function() {
+        Module._run_fmv_with_frequency_provider(document.body.clientWidth, document.body.clientHeight);
+    },
+
+    printErr: function(message) {
+        console.error(Array.prototype.slice.call(arguments).join(' '));
+    },
+
+    print: function(message) {
+        console.log(Array.prototype.slice.call(arguments).join(' '));
+    },
+
+    canvas: document.getElementById('module'),
+
+    setStatus: function(message) {
+        var status = document.getElementById('status');
+        if(status) status.innerHTML = message;
+    },
+
+    setStatusDescription: function(message) {
+        var statusDescription = document.getElementById('statusDescription');
+        if(statusDescription) statusDescription.innerHTML = message;
+    },
+
+    totalDependencies: 0,
+
+    monitorRunDependencies: function(left) {
+        this.totalDependencies = Math.max(this.totalDependencies, left);
+
+        if(left) {
+            Module.setStatus('Downloading...');
+            Module.setStatusDescription((this.totalDependencies - left) + '/' + this.totalDependencies);
+        } else {
+            Module.setStatus('Download complete');
+            Module.setStatusDescription('');
+        }
+    }
+};
+
+Module.setStatus('Downloading...');
+
+
+
